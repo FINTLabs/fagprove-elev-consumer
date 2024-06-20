13c28010d46c6f9f5438b92b85487aed1683f721
Hknots <hknots99@gmail.com>
2024-06-20 09:20:30 +0200

    initial commit

---
+++ src/main/java/no/fintlabs/FagproveBackendApplication.java
@@ -0,0 +1,13 @@
+package no.fintlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FagproveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FagproveBackendApplication.class, args);
+	}
+
+}
