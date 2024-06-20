1f99d16e0a3bc70b22cbd860e297c8924b79f8de
Hknots <hknots99@gmail.com>
2024-06-20 09:24:28 +0200

    add Elev entity

---
+++ src/main/java/no/fintlabs/elev/Elev.java
@@ -0,0 +1,33 @@
+package no.fintlabs.elev;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Entity
+public class Elev {
+
+    @Id
+    @Column(updatable = false, nullable = false, unique = true)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String birthNumber;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String mobileNumber;
+
+    @Column
+    private String averageGrades;
+
+}
