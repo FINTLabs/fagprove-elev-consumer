1c5647315019c456f88e8956cbd887f8b0eb1039
Hknots <hknots99@gmail.com>
2024-06-20 09:45:54 +0200

    add structure of actionlogs

---
+++ src/main/java/no/fintlabs/action/ActionKafkaProducer.java
@@ -0,0 +1,12 @@
+package no.fintlabs.action;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActionKafkaProducer {
+
+    public void publishAction(Action action) {
+        // TODO: Implement later
+    }
+
+}
