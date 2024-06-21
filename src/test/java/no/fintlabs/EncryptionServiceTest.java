1619602f774fdaaa7f00600ca145a1fc8bf352b0
Hknots <hknots99@gmail.com>
2024-06-21 09:56:57 +0200

    add EncryptionService test

---
+++ src/test/java/no/fintlabs/EncryptionServiceTest.java
@@ -0,0 +1,38 @@
+package no.fintlabs;
+
+import no.fintlabs.exception.EncryptionException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class EncryptionServiceTest {
+
+    @InjectMocks
+    private EncryptionService encryptionService;
+
+    private String originalString;
+
+    @BeforeEach
+    void setUp() {
+        originalString = "I want to be encrypted!";
+    }
+
+    @Test
+    public void encryptAndDecrypt() throws EncryptionException {
+        String encryptedString = encryptionService.encrypt(originalString);
+        String decryptedString = encryptionService.decrypt(encryptedString);
+        assertEquals(originalString, decryptedString, "encrpytion and decryption works");
+    }
+
+    @Test
+    public void encryptDoesntMatchOriginalString() throws EncryptionException {
+        String encryptedString = encryptionService.encrypt(originalString);
+        assertNotEquals(originalString, encryptedString, "encryption alters the original string");
+    }
+}
