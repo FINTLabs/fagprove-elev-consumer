d87f7d73df68d88b929bdc4b96783fb3be84b225
Hknots <hknots99@gmail.com>
2024-06-21 08:44:52 +0200

    add ElevEncryptionService

--- src/main/java/no/fintlabs/elev/ElevEncryptionService.java
+++ src/main/java/no/fintlabs/elev/ElevEncryptionService.java
@@ -1,13 +1,40 @@
 package no.fintlabs.elev;
 
+import lombok.RequiredArgsConstructor;
+import no.fintlabs.EncryptionService;
+import no.fintlabs.exception.EncryptionException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ElevEncryptionService {
 
+    private final EncryptionService encryptionService;
+
+    private ElevEntity processEncryption(ElevEntity elev, EncryptionFunction<String, String> function) {
+        try {
+            elev.setName(function.apply(elev.getName()));
+            elev.setEmail(function.apply(elev.getEmail()));
+            elev.setBirthNumber(function.apply(elev.getBirthNumber()));
+            elev.setMobileNumber(function.apply(elev.getMobileNumber()));
+            elev.setAverageGrades(function.apply(elev.getAverageGrades()));
+            return elev;
+        } catch (EncryptionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public ElevEntity encrypt(ElevEntity elev) {
-        // TODO: Implement encryption
-        return null;
+        return processEncryption(elev, encryptionService::encrypt);
+    }
+
+    public ElevEntity decrypt(ElevEntity elev) {
+        return processEncryption(elev, encryptionService::decrypt);
+    }
+
+    @FunctionalInterface
+    public interface EncryptionFunction<T, R> {
+        R apply(T t) throws EncryptionException;
     }
 
 }
