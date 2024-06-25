01d9fe444ebfd8a153d0b113a1c771f5e2802b51
Hknots <hknots99@gmail.com>
2024-06-25 17:42:49 +0200

    ensure topic

--- src/main/java/no/fintlabs/action/ActionLogKafkaProducer.java
+++ src/main/java/no/fintlabs/action/ActionLogKafkaProducer.java
@@ -4,6 +4,7 @@ import no.fintlabs.kafka.entity.EntityProducer;
 import no.fintlabs.kafka.entity.EntityProducerFactory;
 import no.fintlabs.kafka.entity.EntityProducerRecord;
 import no.fintlabs.kafka.entity.topic.EntityTopicNameParameters;
+import no.fintlabs.kafka.entity.topic.EntityTopicService;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -11,14 +12,18 @@ import java.util.UUID;
 @Service
 public class ActionLogKafkaProducer {
 
+    private final static long RETENTION_TIME_IN_MILLIS = 86400000;
+
     private final EntityProducer<ActionLog> actionLogEntityProducer;
     private final EntityTopicNameParameters topicNameParameters;
 
-    public ActionLogKafkaProducer(EntityProducerFactory entityProducerFactory) {
+    public ActionLogKafkaProducer(EntityProducerFactory entityProducerFactory, EntityTopicService entityTopicService) {
         actionLogEntityProducer = entityProducerFactory.createProducer(ActionLog.class);
         topicNameParameters = EntityTopicNameParameters.builder()
                 .resource("actionlog")
                 .build();
+        entityTopicService.ensureTopic(topicNameParameters, RETENTION_TIME_IN_MILLIS);
+
     }
 
     public void publishAction(ActionLog actionLog) {
